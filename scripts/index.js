352f7678a8e416c1bbbb707d9e7921e9fe5b6330
Dmitri Oleinic <dmitryoleinyc@yandex.ru>
2024-04-22 22:03:31 +0200

    added alt text for image

--- scripts/index.js
+++ scripts/index.js
@@ -6,28 +6,28 @@ const placeList = document.querySelector(".places__list");
 
 // @todo: Функция удаления карточки
 function deleteCard(event) {
-  const card = event.target.closest('.card');
+  const card = event.target.closest(".card");
   card.remove();
 }
 
 // @todo: Функция создания карточки
-function addCard(item) {
+function addCard(item, onDeleteCard) {
   const newCard = cardTemplate.content.cloneNode(true);
   const title = newCard.querySelector(".card__title");
   title.innerText = item.name;
   const photo = newCard.querySelector(".card__image");
   photo.setAttribute("src", item.link);
+  photo.setAttribute("alt", item.name);
 
   // Находим кнопку удаления и добавляем обработчик событий
   const deleteButton = newCard.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", onDeleteCard);
 
   return newCard;
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (item) {
-  const card = addCard(item);
+  const card = addCard(item, deleteCard);
   placeList.appendChild(card);
 });
-
