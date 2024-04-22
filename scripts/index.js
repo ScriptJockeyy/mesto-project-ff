ae0ea796ef0d2ed369b79b93070952f5ea6f64f9
Dmitri Oleinic <dmitryoleinyc@yandex.ru>
2024-04-22 18:31:58 +0200

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template");
+
 // @todo: DOM узлы
 
+const placeList = document.querySelector(".places__list");
+console.log(placeList);
 // @todo: Функция создания карточки
+function addCard(item) {
+  const newCard = cardTemplate.content.cloneNode(true);
+  const title = newCard.querySelector(".card__title");
+  title.innerText = item.name;
+  const photo = newCard.querySelector(".card__image");
+  photo.setAttribute("src", item.link);
+  return newCard;
+}
+
+initialCards.forEach(function (item) {
+  const card = addCard(item);
+  placeList.appendChild(card);
+});
 
 // @todo: Функция удаления карточки
 
