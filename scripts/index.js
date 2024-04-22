46a0018260f407826e818b0a2f031fec163a4e9f
Dmitri Oleinic <dmitryoleinyc@yandex.ru>
2024-04-22 20:25:10 +0200

    function_delete_cards_added

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
 // @todo: Темплейт карточки
-
 const cardTemplate = document.querySelector("#card-template");
 
 // @todo: DOM узлы
-
 const placeList = document.querySelector(".places__list");
-console.log(placeList);
+
+// @todo: Функция удаления карточки
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
+
 // @todo: Функция создания карточки
 function addCard(item) {
   const newCard = cardTemplate.content.cloneNode(true);
@@ -13,14 +17,17 @@ function addCard(item) {
   title.innerText = item.name;
   const photo = newCard.querySelector(".card__image");
   photo.setAttribute("src", item.link);
+
+  // Находим кнопку удаления и добавляем обработчик событий
+  const deleteButton = newCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
   return newCard;
 }
 
+// @todo: Вывести карточки на страницу
 initialCards.forEach(function (item) {
   const card = addCard(item);
   placeList.appendChild(card);
 });
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
